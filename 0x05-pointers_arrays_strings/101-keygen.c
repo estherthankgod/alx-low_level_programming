f46ca61db87cd552dbce6ac13601639d2a01317d
estherthankgod <tripcy18@gmail.com>
2022-10-24 12:06:12 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -40,8 +40,10 @@ break;
 for (index = 0; password[index]; index++)
 {
 if (password[index] >= (33 + diff_half2))
-break;
 {
+password[index] -= diff_half2;
+break;
+}
 }
 }
 printf("%s", password);
