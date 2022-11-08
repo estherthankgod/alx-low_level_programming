774c2c39945fcc65fb437efd14e2ebfa1c5a1f31
estherthankgod <tripcy18@gmail.com>
2022-11-08 14:56:36 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,7 +7,7 @@ int a[5];
 int *p;
 
 a[2] = 1024;
-p = &n;
+ p = &n;
 /*
  * write your line of code here...
  * Remember:
@@ -21,3 +21,4 @@ p[5] = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
+
