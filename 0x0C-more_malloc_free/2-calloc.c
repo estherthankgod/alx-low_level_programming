7578d568ffb8825bf1e0a558344960801096010c
estherthankgod <tripcy18@gmail.com>
2022-11-10 13:39:58 -0800

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,12 +15,11 @@ char *p;
 unsigned int i;
 
 if (nmemb == 0 || size == 0)
-return(NULL);
+return (NULL);
 
 p = malloc(nmemb * size);
-
-if (p == NULL);
-return(NULL);
+if (p == NULL)
+return (NULL);
 
 for (i = 0; i < (nmemb * size); i++)
 p[i] = 0;
